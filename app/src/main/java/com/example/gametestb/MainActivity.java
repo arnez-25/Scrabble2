d6024cc0a78a883a4e388e2a8e9e6137cca80465
Jacob <arnez25@up.edu>
2023-03-02 13:16:09 -0800

    Initial commit

---
+++ app/src/main/java/com/example/gametestb/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.gametestb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.GameFramework.GameMainActivity;
+import com.example.GameFramework.LocalGame;
+import com.example.GameFramework.gameConfiguration.GameConfig;
+import com.example.GameFramework.infoMessage.GameState;
+
+public class MainActivity extends GameMainActivity {
+
+    private final static String TAG = "MainActivity";
+    @Override
+    public GameConfig createDefaultConfig(){
+        return null;
+
+    }
+    /**
+     * createLocalGame
+     *
+     * Creates a new game that runs on the server tablet,
+     * @param gameState
+     * 				the gameState for this game or null for a new game
+     *
+     * @return a new, game-specific instance of a sub-class of the LocalGame
+     *         class.
+     */
+    @Override
+    public LocalGame createLocalGame(GameState gameState){
+        return null;
+    }
+
+    /**
+     * saveGame, adds this games prepend to the filename
+     *
+     * @param gameName
+     * 				Desired save name
+     * @return String representation of the save
+     */
+    @Override
+    public GameState saveGame(String gameName){
+        return super.saveGame(getGameString(gameName));
+    }
+
+    /**
+     * loadGame, adds this games prepend to the desire file to open and creates the game specific state
+     * @param gameName
+     * 				The file to open
+     * @return The loaded GameState
+     */
+    @Override
+    public GameState loadGame(String gameName){
+        return null;
+    }
+}
